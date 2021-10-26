34ead68972795a6cd462b07f1b3ffab9b640078f
Denis Zvonarić <83345879+dzvonari@users.noreply.github.com>
2021-10-26 14:51:45 +0200

    isset

--- vjezba2.sql
+++ vjezba2.sql
@@ -10,6 +10,23 @@ select * from autor;
 
 show tables;
 
+select * from country;
+
+show tables;
+
+insert into country (Name,Continent) values
+('Kosovo','Europe');
+update country set Code='KOS' where Name='Kosovo';
+
+select * from city;
+
+insert into city (Name,CountryCode,Population) values 
+('Priština donja','KOS','25');
+
+select * from city where name='Priština';
+
+update city set name='Priština' where ID=4080;
+
 update autor set datumrodenja='1983-02-07' where sifra=3; 
 
 select * from izdavac;
---
+++ vjezba3.sql
@@ -0,0 +1,40 @@
+select a.naziv as grupa, b.naziv as smjer,
+concat(d.ime,' ',d.prezime) as predavac,
+concat(g.ime,' ',g.prezime) as polaznik
+from grupa a 
+inner join smjer b on a.smjer=b.sifra
+inner join predavac c on a.predavac=c.sifra
+inner join osoba d on c.osoba=d.sifra
+inner join clan e on e.grupa=a.sifra
+inner join polaznik f on f.sifra=e.polaznik 
+inner join osoba g on g.sifra=f.osoba
+; 
+
+select a.ime, a.prezime
+from osoba a  inner join polaznik b on a.sifra=b.osoba
+inner join clan c on c.polaznik=b.sifra 
+inner join grupa d on b.sifra=c.grupa 
+inner join smjer e on e.sifra=d.smjer
+where d.naziv='PP24'
+;
+
+select * from grupa where sifra=4;
+
+select * from polaznik
+
+select * from osoba;
+select * from autor;
+show tables;
+
+select b.naslov
+from autor a inner join katalog b on  b.autor=a.sifra
+where a.ime='August' and a.prezime='Šenoa';
+
+select d.naziv, b.mjesto
+from katalog b inner join izdavac c on b.izdavac=c.sifra
+inner join mjesto d on d.sifra=b.mjesto
+where c.naziv not like 'H%';
+
+select p.productName, s.productLine 
+from products p inner join productlines s on p.productLine=s.productLine 
+;
